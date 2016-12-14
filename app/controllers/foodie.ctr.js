27a5eb5e9696c6ce634968108b172b9027be82f5
asimonia <alex.simonian@gmail.com>
2016-12-13 18:49:10 -0600

    Add sidenav, form entry, toast

--- app/controllers/foodie.ctr.js
+++ app/controllers/foodie.ctr.js
@@ -1,8 +1,37 @@
 "use strict";
 
-app.controller("foodieCtrl", function($scope, $http, foodieFactory) {
+app.controller("foodieCtrl", function($scope, $http, foodieFactory, $mdSidenav, $mdToast) {
 
 	foodieFactory.getFood().then(function(fooditems) {
 		$scope.fooditems = fooditems.data;
-	})
+	});
+
+	var contact = {
+		name: "Alex Simonian",
+		phone: "(555) 555-5555",
+		email: "alex.simonian@gmail.com"
+	}
+
+	$scope.openSidebar = function() {
+		$mdSidenav('left').open();
+	}
+
+	$scope.closeSidebar = function() {
+		$mdSidenav('left').close();
+	}
+
+	$scope.saveFooditem = function(fooditem) {
+		if(fooditem) {
+			fooditem.contact = contact
+			$scope.fooditems.push(fooditem);
+			$scope.fooditem = {};
+			$scope.closeSidebar();
+			$mdToast.show(
+				$mdToast.simple()
+					.content(`${fooditem.title} saved!`)
+					.position('top, right')
+					.hideDelay(3000)
+			);
+		}
+	}
 });
