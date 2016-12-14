0a583d5b0a370bb1276dc35b478d4bfc245d517e
asimonia <alex.simonian@gmail.com>
2016-12-14 11:25:44 -0600

    Added watcher

--- app/controllers/foodie.new.ctr.js
+++ app/controllers/foodie.new.ctr.js
@@ -1,11 +1,26 @@
 "use strict";
 
-app.controller("newFoodieCtrl", function($mdSidenav, $timeout, $mdDialog, foodieFactory) {
+app.controller("newFoodieCtrl", function($scope, $state, $mdSidenav, $timeout, $mdDialog, foodieFactory) {
 
 	var vm = this;
+	vm.closeSidebar = closeSidebar;
 
 	$timeout(function() {
 		$mdSidenav('left').open();	
 	});
+
+	$scope.$watch('vm.sidenavOpen', function(sidenav) {
+		if(sidenav === false) {
+			$mdSidenav('left')
+				.close()
+				.then(function() {
+					$state.go('fooditems');
+				});
+		}
+	});
+
+	function closeSidebar() {
+		vm.sidenavOpen = false;
+	}
 
 });
